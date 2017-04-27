6d70c13f2b083b07dd639d2bd70ab14edca627da
Vitaliy Kravchenko <knkobox@gmail.com>
2017-04-27 15:39:10 +0300

    First commit

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->post('/hello', function (Request $request, Response $response) {
+    $username = $request->getParam('username');
+    $data = array('message' => "Hello $username");
+    $json_string = json_encode($data, JSON_PRETTY_PRINT);
+    $response->getBody()->write("$json_string");
+    return $response;
+});
+$app->run();
