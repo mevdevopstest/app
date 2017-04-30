3a462220a6c0d5fa5a36b35a6ad45ac6c5064be1
Vitaliy Kravchenko <knkobox@gmail.com>
2017-04-30 15:58:42 +0300

    Add docker stuff

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 $app->post('/hello', function (Request $request, Response $response) {
